c4a8db13ad7bf939b81db6f1be62496e546bd953
Vince Carey <stvjc@channing.harvard.edu>
2016-08-08 09:22:19 -0400

    now passing check at 0.0.2, one NOTE on data size

---
+++ R/ensViz.R
@@ -0,0 +1,34 @@
+
+ensViz = function( eid, sym="CRISPLD2", edbnm = "EnsDb.Hsapiens.v75", ideo=TRUE, genome="hg19", ...) {
+#
+# use Gviz to display a gene model (exon layout per transcript)
+# derived from Ensembl
+#
+# a 'transcript' column must be present in any GRanges passed
+# to GeneRegionTrack to get this type of display
+#
+# if ideo=TRUE, an ideogram track will be fetched and displayed
+# this will be cached at the session level, first run can take
+# some time
+#
+  require(edbnm, character.only=TRUE)
+  edb = get(edbnm)
+  if (!missing(eid)) {
+    sym = select(edb, keys=eid, keytype="GENEID", columns="GENENAME")
+    ex = ensembldb::exonsBy(edb, by="tx", filter=GeneidFilter(eid))
+    }
+  else if (!missing(sym) || is.character(sym)) {
+    eid = select(edb, keys=sym, keytype="GENENAME", columns="GENEID")
+    ex = ensembldb::exonsBy(edb, by="tx", filter=GeneidFilter(eid$GENEID[1]))
+    }
+  ex = unlist(ex)
+  ex$transcript = ex$tx_id
+  seqlevelsStyle(ex) = "UCSC"
+  cn = as.character(seqnames(ex))[1] # check representative?
+  if (!all(cn == as.character(seqnames(ex)))) warning("multiple seqnames found")
+  itr = NULL
+  if (ideo) itr = IdeogramTrack(chromosome=cn, genome=genome)
+  plotTracks(c(itr, GenomeAxisTrack(), GeneRegionTrack(ex, name=paste0(sym, 
+    " (", cn, ")"),
+     transcriptAnnotation="transcript")))
+}
